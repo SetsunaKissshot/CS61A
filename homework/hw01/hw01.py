248306f42fc9764333f6b2ade7e7378fa326c501
SetsunaKissshot <58714740+SetsunaKissshot@users.noreply.github.com>
2024-12-12 10:52:08 +0800

    Finish hw01

--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return min(i*i + j*j, i*i + k*k, j*j + k*k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,13 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    factor = 1
+    num = 1
+    while (num < n):
+        if (n % num == 0):
+            factor = num
+        num += 1
+    return factor
 
 
 def hailstone(n):
@@ -88,4 +95,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    count = 1
+    while (n != 1):
+        count += 1
+        print(n)
+        if (n % 2 == 0):
+            n //= 2
+        else:
+            n = n * 3 + 1
 
+    print(n)
+    return count
